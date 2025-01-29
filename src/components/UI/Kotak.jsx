57e5e3f797b4645acf27d195b928df7eeb84b99d
iplesples <ceo.iplesples@gmail.com>
2025-01-29 23:57:05 +0700

    layout slide GU

---
+++ src/components/UI/Kotak.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Kotak = () => {
+  return (
+    <div className='mt-10 w-56 h-36 bg-amber-300'>5</div>
+  )
+}
+
+export default Kotak
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,43 +1,42 @@
-//vertical swiper
-
 // import Swiper core and required modules
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+// import Swiper core and required modules
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css'; // Import Swiper CSS
-import Pages from './pages/Pages';
 
-function App() {
+function Pages() {
   return (
     <div className="h-screen flex justify-center items-center">
 
       <Swiper
-        direction="horizontal" // Mengatur arah swipe ke vertikal
+        direction="vertical" // Mengatur arah swipe ke vertikal
         modules={[Pagination, Navigation]} // Menggunakan Pagination dan Navigation
         spaceBetween={10} // Jarak antar slide
         slidesPerView={1} // Menampilkan satu slide per tampilan
         loop={true} // Mengaktifkan looping
         pagination={{ clickable: true }} // Mengaktifkan pagination yang bisa diklik
         navigation // Mengaktifkan tombol navigasi
-        className="w-sreen h-screen" // Menetapkan ukuran Swiper
+        className="w-[100vw] h-[100vh]" // Menetapkan ukuran Swiper
       >
         <SwiperSlide className="bg-blue-500 text-white flex items-center justify-center h-full">
-        <Pages />
+          <div className="text-xl">Slide 1</div>
         </SwiperSlide>
         <SwiperSlide className="bg-green-500 text-white flex items-center justify-center h-full">
-        <Pages />
+          <div className="text-xl">Slide 2</div>
         </SwiperSlide>
         <SwiperSlide className="bg-red-500 text-white flex items-center justify-center h-full">
-        <Pages />
+          <div className="text-xl">Slide 3</div>
         </SwiperSlide>
         <SwiperSlide className="bg-yellow-500 text-white flex items-center justify-center h-full">
-        <Pages />
+          <div className="text-xl">Slide 4</div>
         </SwiperSlide>
         <SwiperSlide className="bg-purple-500 text-white flex items-center justify-center h-full">
-        <Pages />
+          <div className="text-xl">Slide 5</div>
         </SwiperSlide>
       </Swiper>
     </div>
   );
 }
 
-export default App;
+export default Pages;
